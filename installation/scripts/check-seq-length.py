e1155f91c61d4d48bb4d73822e6b2b76da918330
AR Riyaz <arriyaz.nstu@gmail.com>
2023-08-02 22:11:38 +0600

    Added new features
    - ability to work on multiple fasta file
    - reformat output

--- installation/scripts/check-seq-length.py
+++ installation/scripts/check-seq-length.py
@@ -2,16 +2,26 @@
 
 from Bio import SeqIO
 import argparse
+import os
 
-def check_sequence_length(input_file):
-    for record in SeqIO.parse(input_file, "fasta"):
-        seq_id = record.id
-        seq_length = len(record.seq)
-        print(f"{seq_id}\t{seq_length}")
+def check_sequence_length(input_files):
+
+    for input_file in input_files:
+        
+        # Extract filename
+        filename = os.path.basename(input_file)
+
+        for record in SeqIO.parse(input_file, "fasta"):
+            seq_id = record.id
+            seq_length = len(record.seq)
+            print(f"{filename}\t{seq_id}\t{seq_length}")
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Check the length of sequences in a FASTA file.")
-    parser.add_argument("-i", "--input", dest="input_file", required=True, help="Input FASTA file.")
+    parser = argparse.ArgumentParser(
+        prog="check-seq-length",
+        description="Check the length of sequences in FASTA file[s].")
+    parser.add_argument("-i", "--input", nargs="+", dest="input_file", required=True, 
+                        help="Input FASTA file.")
     args = parser.parse_args()
 
     check_sequence_length(args.input_file)
